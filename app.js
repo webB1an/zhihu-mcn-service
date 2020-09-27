7a1d28aa2a92135b7f412c7d16707eb97827d324
webB1an <webB1anyaoyao@gmail.com>
2020-09-27 16:32:44 +0800

    feat: add eslint config

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
+
+dotenv.config({ path: './dev.env' })
 
 const app = express()
+
+// parse post request
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 
 app.listen(3000, () => {
   console.log('server is running')
