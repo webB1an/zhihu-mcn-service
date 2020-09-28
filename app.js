ea48902d2d849f00150c0ae65c00b873d61d8bbe
webB1an <webB1anyaoyao@gmail.com>
2020-09-28 10:47:45 +0800

    feat: add dotenv debugger

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ dotenv.config({ path: './dev.env' })
 
 const app = express()
 
+console.log('==================================')
+console.log('NODE_ENV', process.env.NODE_ENV)
+console.log('PORT', process.env.PORT)
+console.log('MONGODB_URL', process.env.MONGODB_URL)
+console.log('==================================')
+
 // parse post request
 app.use(bodyParser.json({ limit: '50mb' }))
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 
-app.listen(3000, () => {
-  console.log('server is running')
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running at port: ${process.env.PORT}`)
 })
